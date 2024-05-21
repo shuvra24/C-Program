b517cfcbd563e2f3a8645d857a952bebe51e936c
shuvra_roy <shuvraroy21@gmail.com>
2024-05-21 19:10:29 +0600

    addde a new file length of string

---
+++ week 5/module 17/length_of_string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int fun(char *s,int i){
+    if(s[i]=='\0') return 0;
+    int l=fun(s,i+1);
+    return l+1;
+}
+int main()
+{
+    char s[]="bangladesh";
+    int length=fun(s,0);
+    printf("%d",length);
+    return 0;
+}
